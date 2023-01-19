8f22d4ebf914298aae534c7fad187bccbcd515cb
Jasper <49565505+wluxie@users.noreply.github.com>
2023-01-19 11:34:14 -0800

    397 & 2487

---
+++ 397.integer-replacement.py
@@ -0,0 +1,18 @@
+#
+# @lc app=leetcode id=397 lang=python3
+#
+# [397] Integer Replacement
+#
+
+# @lc code=start
+class Solution:
+    def integerReplacement(self, n: int) -> int:
+        if n == 1:
+            return 0
+        if n % 2 == 0:
+            return 1 + self.integerReplacement(n // 2)
+        else:
+            return 1 + min(self.integerReplacement(n + 1), self.integerReplacement(n - 1))
+        
+# @lc code=end
+
