bb938930e108f47f8911d3fa8e2561c917717fe2
Jasper <49565505+wluxie@users.noreply.github.com>
2023-01-18 18:55:35 -0800

    974

---
+++ 974.subarray-sums-divisible-by-k.cpp
@@ -0,0 +1,30 @@
+/*
+ * @lc app=leetcode id=974 lang=cpp
+ *
+ * [974] Subarray Sums Divisible by K
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int subarraysDivByK(vector<int>& nums, int k) {
+        int prefixMod = 0, result = 0;
+
+        // There are k mod groups 0...k-1.
+        vector<int> modGroups(k);
+        modGroups[0] = 1;
+
+        for (int num : nums) {
+            // Take modulo twice to avoid negative remainders.
+            prefixMod = (prefixMod + num % k + k) % k;
+            // Add the count of subarrays that have the same remainder as the current
+            // one to cancel out the remainders.
+            result += modGroups[prefixMod];
+            modGroups[prefixMod]++;
+        }
+
+        return result;
+    }
+};
+// @lc code=end
+
